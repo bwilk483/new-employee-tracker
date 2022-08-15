581d801ce41987bcc85f18144ee64e99919d3d76
Bryan Wilkerson <bwilk483@gmail.com>
2022-08-15 14:00:32 -0500

    mysql database set up

---
+++ db/db.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS new_employee_tracker;
+CREATE DATABASE new_employee_tracker;
+USE new-employee_tracker;
--- db/schema.sql
+++
@@ -1,3 +0,0 @@
-
-DROP DATABASE IF EXISTS employeedb;
-CREATE DATABASE employeedb;
